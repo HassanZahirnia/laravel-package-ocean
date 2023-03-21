4ac1c45ffd551a0bb88837d631dc31b0bee72dad
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-03-21 20:00:40 +0330

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,33 @@ export default defineNuxtConfig({
             },
         },
     },
+    app: {
+        head: {
+            title: 'Laravel Package Ocean - Discover new & useful Laravel packages',
+            meta: [
+                {
+                    name: 'description',
+                    content: 'Discover new & useful Laravel packages',
+                },
+                {
+                    name: 'msapplication-TileColor',
+                    content: '#2b5797',
+                },
+                {
+                    name: 'theme-color',
+                    content: '#ffffff',
+                },
+            ],
+            link: [
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png?v=w1dBNxT7Wg' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png?v=w1dBNxT7Wg' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png?v=w1dBNxT7Wg' },
+                { rel: 'manifest', href: '/site.webmanifest?v=w1dBNxT7Wg' },
+                { rel: 'mask-icon', href: '/safari-pinned-tab.svg?v=w1dBNxT7Wg', color: '#5bbad5' },
+                { rel: 'shortcut icon', href: '/favicon.ico?v=w1dBNxT7Wg' },
+            ],
+        },
+    },
     modules: [
         'nuxt-simple-sitemap',
     ],
