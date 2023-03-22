06b45c2bb65a2f394504334c9b4da43514a750e3
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-03-22 19:43:20 +0330

    Add stats to landing hero section

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,19 @@
 
 <template>
     <section class="relative z-10 mx-auto w-full max-w-screen-xl">
+        <!-- Ellipse arc -->
         <div
             class="pointer-events-none absolute
             top-[-70vw] right-[-15rem] -z-10
             h-[calc(100vw+10rem)]
             w-[calc(100vw+10rem)]
             scale-y-75 scale-x-150
-            rounded-full bg-white/30 dark:bg-[#173563]/10
+            rounded-full bg-white/20
+            transition duration-300
+            dark:bg-[#173563]/10
             "
             />
+        <!-- Purple blur -->
         <div
             class="pointer-events-none absolute -top-10 right-20 -z-20 h-[30rem]
             w-[30rem] rounded-full bg-[#E6D0FC] blur-[200px]
@@ -19,6 +23,7 @@
             dark:bg-[#63388E]
             "
             />
+        <!-- Blue blur -->
         <div
             class="pointer-events-none absolute -bottom-40 left-20 -z-30 h-[30rem]
             w-[30rem] rounded-full bg-[#60A9FF]/50 blur-[200px]
@@ -27,6 +32,7 @@
             "
             />
         <div class="relative">
+            <!-- Fluid -->
             <img
                 src="@/assets/images/fluid-shape.webp"
                 width="auto"
@@ -37,23 +43,86 @@
                 />
             <div class="pt-36">
                 <div
-                    class="mx-auto w-full max-w-3xl rounded-3xl
-                    bg-white/50 p-14 text-center
+                    class="mx-auto w-full max-w-4xl rounded-3xl
+                    bg-white/20 px-10 pt-16 pb-10 text-center
                     shadow-[8.05051px_24.1515px_89.4501px_-11.6285px_rgba(22,52,80,0.05)]
-                    backdrop-blur-xl
+                    backdrop-blur-xl transition
+                    duration-300
                     dark:bg-[#110E26]/50
                     "
                     >
-                    <div class="text-4xl font-bold text-[#040404] dark:text-white">
+                    <div
+                        class="text-4xl font-bold text-[#040404] transition
+                        duration-300 dark:text-white
+                        "
+                        >
                         Discover new Laravel packages.
                     </div>
                     <div
                         class="mx-auto max-w-lg pt-5 text-xl
                         font-medium text-[#6A7789]
+                        transition duration-300
                         dark:text-[#96A5BB]
                         "
                         >
                         Our goal is to help the Laravel community to find new & useful Laravel packages in one place.
+                    </div>
+                    <div
+                        class="mx-20 mt-11
+                        flex justify-center gap-24
+                        border-t border-t-[#6A7789]/20
+                        pt-9
+                        transition duration-300 dark:border-t-[#627288]/20
+                        "
+                        >
+                        <div class="space-y-0.5">
+                            <div
+                                class="text-4xl font-bold text-[#4B6CFF]
+                                transition duration-300
+                                "
+                                >
+                                294
+                            </div>
+                            <div
+                                class="font-medium text-[#827F98] transition
+                                duration-300 dark:text-[#96A5BB]
+                                "
+                                >
+                                Packages
+                            </div>
+                        </div>
+                        <div class="space-y-0.5">
+                            <div
+                                class="text-4xl font-bold text-[#4B6CFF]
+                                transition duration-300
+                                "
+                                >
+                                84
+                            </div>
+                            <div
+                                class="font-medium text-[#827F98] transition
+                                duration-300 dark:text-[#96A5BB]
+                                "
+                                >
+                                Authors
+                            </div>
+                        </div>
+                        <div class="space-y-0.5">
+                            <div
+                                class="text-4xl font-bold text-[#4B6CFF]
+                                transition duration-300
+                                "
+                                >
+                                13
+                            </div>
+                            <div
+                                class="font-medium text-[#827F98] transition
+                                duration-300 dark:text-[#96A5BB]
+                                "
+                                >
+                                Categories
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
