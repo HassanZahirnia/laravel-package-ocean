ce4e02551eedeaee27c4eb5bb6d5af8599eb0cdc
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-03-23 16:02:27 +0330

    Organize some of the components

---
+++ components/landing/background.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- Ellipse arc -->
+    <div
+        class="pointer-events-none absolute top-[-70vw] left-1/2
+        -z-10 h-[calc(100vw+10rem)] w-[calc(100vw+10rem)] -translate-x-1/2
+        scale-y-75
+        scale-x-150
+        select-none rounded-full
+        bg-white/20 transition
+        duration-300 dark:bg-[#173563]/10
+        min-[2000px]:top-[-80vw]
+        "
+        />
+    <!-- Purple blur -->
+    <div
+        class="pointer-events-none absolute top-[-5vw]
+        right-[calc(10rem-10vw)] -z-20 h-[calc(10rem+25vw)] w-[calc(10rem+25vw)]
+        select-none rounded-full bg-[#e2c9fc] blur-[calc(5rem+13vw)]
+        transition duration-300
+        dark:bg-[#63388E]
+        "
+        />
+    <!-- Blue blur -->
+    <div
+        class="pointer-events-none absolute bottom-[-20vw]
+        left-[calc(10rem-10vw)] -z-30 h-[calc(10rem+25vw)] w-[calc(10rem+25vw)]
+        select-none rounded-full bg-[#60A9FF]/50 blur-[calc(5rem+13vw)]
+        transition duration-300
+        dark:bg-[#0A7BFF]/30
+        "
+        />
+</template>
---
+++ components/landing/introduction.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="relative">
+        <!-- Fluid -->
+        <img
+            src="@/assets/images/fluid-shape.webp"
+            width="auto"
+            height="auto"
+            alt=""
+            class="pointer-events-none absolute top-5 right-1/2 z-[-1] w-60
+            translate-x-1/2 select-none sm:-top-5 lg:-top-10 lg:w-72
+            "
+            />
+        <div class="px-5 pt-40 sm:px-10 sm:pt-36">
+            <div
+                class="mx-auto w-full max-w-3xl rounded-3xl bg-white/20
+                px-10 pt-16 pb-10 text-center shadow-[8.05051px_24.1515px_89.4501px_-11.6285px_rgba(22,52,80,0.05)]
+                backdrop-blur-xl
+                transition duration-300
+                dark:bg-[#110E26]/50
+                lg:max-w-4xl
+                "
+                >
+                <div
+                    class="text-2xl font-bold text-[#040404] transition duration-300 dark:text-white
+                    sm:text-3xl lg:text-4xl
+                    "
+                    >
+                    Discover new Laravel packages.
+                </div>
+                <div
+                    class="mx-auto max-w-sm pt-5 font-medium text-[#6A7789] transition duration-300
+                    dark:text-[#96A5BB] sm:max-w-md
+                    sm:text-lg lg:max-w-lg
+                    lg:text-xl
+                    "
+                    >
+                    Our goal is to help the Laravel community to find new & useful Laravel packages in one place.
+                </div>
+                <div
+                    class="mt-11 flex flex-wrap
+                    justify-center gap-y-10 gap-x-24 border-t border-t-[#6A7789]/20
+                    pt-9 transition
+                    duration-300
+                    dark:border-t-[#627288]/20 sm:mx-10 md:mx-20
+                    "
+                    >
+                    <div class="space-y-0.5">
+                        <div
+                            class="text-3xl font-bold text-[#4B6CFF] transition
+                            duration-300 lg:text-4xl
+                            "
+                            >
+                            294
+                        </div>
+                        <div
+                            class="text-sm font-medium text-[#827F98]
+                            transition duration-300
+                            dark:text-[#96A5BB] lg:text-base
+                            "
+                            >
+                            Packages
+                        </div>
+                    </div>
+                    <div class="space-y-0.5">
+                        <div
+                            class="text-3xl font-bold text-[#4B6CFF] transition
+                            duration-300 lg:text-4xl
+                            "
+                            >
+                            84
+                        </div>
+                        <div
+                            class="text-sm font-medium text-[#827F98]
+                            transition duration-300
+                            dark:text-[#96A5BB] lg:text-base
+                            "
+                            >
+                            Authors
+                        </div>
+                    </div>
+                    <div class="space-y-0.5">
+                        <div
+                            class="text-3xl font-bold text-[#4B6CFF] transition
+                            duration-300 lg:text-4xl
+                            "
+                            >
+                            13
+                        </div>
+                        <div
+                            class="text-sm font-medium text-[#827F98]
+                            transition duration-300
+                            dark:text-[#96A5BB] lg:text-base
+                            "
+                            >
+                            Categories
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
