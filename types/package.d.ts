9eb26d2095d8da02b7c4d3a9300d6a75da7143bf
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-03-29 14:44:18 +0330

    Add updated_at date to each package

--- types/package.d.ts
+++ types/package.d.ts
@@ -3,16 +3,38 @@ import { categories } from '@/database/categories'
 export type Category = typeof categories[number]
 
 export type Package = {
+    // Name of the package. Example: 'Laravel Backup'
     name: string
+    
+    // Description of the package. Example: 'A package to backup your Laravel app.'
     description: string
+
+    // Category of the package. Example: 'Dev Ops'
     category: Category
+
+    // Github URL of the package.'
     github: string
+
+    // Composer name of the package. Example: 'spatie/laravel-backup'
     composer: string
+
+    // Author of the package. Example: 'spatie'
     author: string
+
+    // Number of stars on Github. Example: 5194
     stars: number
+
+    // Keywords of the package. Example: ['backup']
     keywords: string[]
+
+    // Date of the first release on packagist. Example: '2015-09-15T15:36:13+00:00'
     first_release_at: string
+
+    // Date of the latest release on packagist. Example: '2023-03-22T02:57:36+00:00'
     latest_release_at: string
+
+    // Date when the package was updated in the database. Example: '2023-03-29T02:57:36+00:00'
+    updated_at: string
 }
 
 export type PackageSortFields = 'first_release_at' | 'latest_release_at' | 'stars'
--- update-database.ts
+++ update-database.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { readFileSync, writeFileSync } from 'node:fs'
 import axios from 'axios'
 import type { Package } from '@/types/package'
@@ -9,7 +10,13 @@ async function updatePackages() {
     const packages: Package[] = eval(packagesFile.split(' = ')[1])
 
     for (let i = 0; i < packages.length; i++) {
-        const { composer } = packages[i]
+        const { composer, updated_at } = packages[i]
+
+        // Skip updating the package if it has been updated in less than a day compared to the current time
+        if (new Date(updated_at) > new Date(Date.now() - 24 * 60 * 60 * 1000)) {
+            console.log(`Skipping ${composer} because it has been updated recently`)
+            continue
+        }
 
         const { data: packagistData } = await axios.get(`https://packagist.org/packages/${composer}.json`)
         const releases = packagistData.package.versions
@@ -26,8 +33,8 @@ async function updatePackages() {
 
         const { data: githubData } = await axios.get(`https://api.github.com/repos/${packages[i].github.substring(19)}`)
         packages[i].stars = githubData.stargazers_count
+        packages[i].updated_at = new Date().toISOString()
 
-        // eslint-disable-next-line no-console
         console.log(`Updated ${composer} with ${packages[i].stars} stars and first release at ${packages[i].first_release_at} and latest release at ${packages[i].latest_release_at}`)
     }
 
