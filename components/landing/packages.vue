b945b7cddbb35451f0a2f329c6adedbf9addf4df
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-04-17 16:52:30 +0330

    Fix emerald background in dark theme

--- components/landing/packages.vue
+++ components/landing/packages.vue
@@ -9,19 +9,13 @@ const isLight = computed(() => colorMode.value === 'sepia' || colorMode.value ==
 
 // Animations
 onMounted(() => {
-    gsap.set('.gsap-emerald-background-blur, .gsap-emerald-background-blur-dark', {
+    gsap.set('.gsap-emerald-background-blur', {
         autoAlpha: 0,
     })
     gsap.fromTo('.gsap-emerald-background-blur', {
         autoAlpha: 0,
     }, {
         autoAlpha: 1,
-        duration: 0.3,
-    })
-    gsap.fromTo('.gsap-emerald-background-blur-dark', {
-        autoAlpha: 0,
-    }, {
-        autoAlpha: 0.5,
         duration: 0.3,
     })
 })
@@ -34,35 +28,30 @@ onMounted(() => {
         "
         >
         <!-- Emerald background -->
-        <img
-            v-show="!colorMode.unknown && isLight"
-            src="@/assets/images/emerald-blur.webp"
-            width="auto"
-            height="auto"
-            alt=""
-            class="gsap-emerald-background-blur pointer-events-none select-none
-            h-[calc(10rem+35vw)] w-[calc(10rem+35vw)]
-            scale-[2.5]
+        <div
+            class="gsap-emerald-background-blur
             absolute -z-40
             top-1/2 right-1/2
             -translate-y-1/2 translate-x-1/2
-            "
-            />
-        <!-- Emerald background for dark theme -->
-        <img
-            v-show="!colorMode.unknown && !isLight"
-            src="@/assets/images/emerald-blur.webp"
-            width="auto"
-            height="auto"
-            alt=""
-            class="gsap-emerald-background-blur-dark pointer-events-none select-none
             h-[calc(10rem+35vw)] w-[calc(10rem+35vw)]
             scale-[2.5]
-            absolute -z-40
-            top-1/2 right-1/2
-            -translate-y-1/2 translate-x-1/2
             "
-            />
+            >
+            <img
+                v-show="!colorMode.unknown"
+                src="@/assets/images/emerald-blur.webp"
+                width="auto"
+                height="auto"
+                alt=""
+                class="pointer-events-none select-none
+                w-full h-full
+                transition duration-300
+                "
+                :class="{
+                    'opacity-60': !isLight,
+                }"
+                />
+        </div>
         <!-- Categories & Packages list -->
         <ClientOnly
             fallback-tag="span"
