f027c827f822d54aaf4ec27fa23c23aa0bb6de23
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-06-06 21:38:17 +0330

    Update query api

--- server/api/query.ts
+++ server/api/query.ts
@@ -1,11 +1,9 @@
-import fs from 'node:fs'
-import type { Package } from '~/types/package'
+import { laravelPackages } from '@/database/packages'
 
 export default defineEventHandler((event) => {
     const query = getQuery(event)
     const githubUrl = query.github
-    const packages: Package[] = Array.from(JSON.parse(fs.readFileSync('database/json/packages.json', 'utf8')))
-    const exists = packages.some(item => item.github === githubUrl)
+    const exists = laravelPackages.some(item => item.github === githubUrl)
     return {
         exists,
     }
