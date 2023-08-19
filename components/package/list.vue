c37a2ad607a4ec251eb7fd9c3f0b569c9fa17c82
514sid <140138716+514sid@users.noreply.github.com>
2023-08-19 13:05:29 +0400

    replace new packages button to component

--- components/package/list.vue
+++ components/package/list.vue
@@ -365,43 +365,12 @@ const categoriesForSelectboxWithAll = [
                     </div>
                 </transition>
                 <!-- New Since Last Visit Button -->
-                <ui-tooltip
+                <ui-new-packages-button
                     v-if="newPackagesSinceLastVisit.length"
-                    :content="newPackagesSinceLastVisit.length + ' New packages were added since your last visit !'"
-                    theme="emerald"
-                    @click.stop.prevent="showNewPackagesSinceLastVisit = !showNewPackagesSinceLastVisit"
-                    >
-                    <div
-
-                        class="py-1.5 px-4 select-none
-                        rounded-full cursor-pointer
-                        transition-all duration-300
-                        flex gap-2 items-center
-                        font-medium
-                        hover:brightness-105
-                        "
-                        :class="{
-                            'bg-slate-300/50 dark:bg-slate-700/50 text-slate-600 dark:text-slate-400': !showNewPackagesSinceLastVisit,
-                            'bg-teal-200/70 text-teal-600 dark:text-teal-500 dark:bg-teal-500/20': showNewPackagesSinceLastVisit,
-                        }"
-                        >
-                        <span class="relative flex h-2.5 w-2.5">
-                            <span
-                                class="animate-ping absolute inline-flex h-full w-full rounded-full bg-teal-400"
-                                :class="{
-                                    'opacity-75': !showNewPackagesSinceLastVisit,
-                                    'opacity-0': showNewPackagesSinceLastVisit,
-                                }"
-                                />
-                            <span class="relative inline-flex rounded-full h-2.5 w-2.5 bg-teal-500" />
-                        </span>
-                        <div class="">
-                            {{ newPackagesSinceLastVisit.length }}
-                            <span class="inline">New Item</span>
-                            <span class="inline">{{ newPackagesSinceLastVisit.length > 1 ? 's' : '' }}</span>
-                        </div>
-                    </div>
-                </ui-tooltip>
+                    :newPackagesCount="newPackagesSinceLastVisit.length"
+                    :showNewPackagesSinceLastVisit="showNewPackagesSinceLastVisit"
+                    @toggle="showNewPackagesSinceLastVisit = !showNewPackagesSinceLastVisit"
+                    />
             </div>
             <div
                 class="xl:flex-1
