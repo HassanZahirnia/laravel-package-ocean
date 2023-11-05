9afeaf3194a143fde70c7c7df70301cbfad38908
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-11-05 20:12:04 +0330

    Add Github package query route to check if package exists in database

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,21 @@
 <?php
 
+use App\Models\Package;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Set the api/query?github=${url} route and check if the package exists in the database by the Github url
+Route::get('/query', function (Request $request) {
+    $package = Package::where('github', $request->string('github'))->first();
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    // If the package exists, return the exists key as true, else return false
+    if ($package) {
+        return [
+            'exists' => true,
+        ];
+    }
+
+    return [
+        'exists' => false,
+    ];
+})->middleware('throttle:60,1');
