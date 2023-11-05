4c86a9879a6d391a07ac1d2b8c1f4a66261bc4f8
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-11-05 20:30:52 +0330

    Delete unused code files and components

--- old-nuxt/composables/states.ts
+++
@@ -1,3 +0,0 @@
-export const useSearch = () => useState<string>('search')
-export const useSelectedCategory = () => useState<string>('selectedCategory', () => '')
-export const useShowOfficialPackages = () => useState<'0' | '1' | ''>('showOfficialPackages', () => '')
