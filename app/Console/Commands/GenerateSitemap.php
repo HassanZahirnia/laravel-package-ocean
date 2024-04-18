2701242fa6f2fb1b5f571d7decf5722b78c859d2
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2024-04-18 21:25:25 +0330

    Add sitemap

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the sitemap';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Sitemap::create()
+            ->add(
+                Url::create('/')
+                    ->setLastModificationDate(now())
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                    ->setPriority(1),
+            )
+            ->writeToFile(public_path('sitemap.xml'));
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Schedule;
 
+// Update the Laravel versions every day
 Schedule::command('app:fetch-laravel-versions')->daily();
+
+// Generate the sitemap every day
+Schedule::command('sitemap:generate')->daily();
