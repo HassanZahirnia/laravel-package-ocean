0cb53cd47fd0db69c231ff5574973eda4b1bdca7
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2024-05-15 15:24:49 +0330

    Add sponsor button to home page

--- resources/views/livewire/home-hero.blade.php
+++ resources/views/livewire/home-hero.blade.php
@@ -21,6 +21,15 @@
                     end: 'bottom bottom',
                 },
             })
+            gsap.to('#hero-section .gsap-sponsor-card', {
+                yPercent: -3,
+                scrollTrigger: {
+                    trigger: 'body',
+                    scrub: 1,
+                    start: '100px 50px',
+                    end: 'bottom bottom',
+                },
+            })
         },
     }"
 >
@@ -53,7 +62,7 @@
                 Laravel packages in one place.
             </div>
             <div
-                class="mt-11 flex flex-wrap justify-around gap-5 border-t border-t-[#6A7789]/20 pt-9 transition duration-300 dark:border-t-[#627288]/20 min-[450px]:gap-14 sm:mx-10 md:mx-20 md:justify-center md:gap-24"
+                class="flex flex-wrap justify-around gap-5 pt-10 transition duration-300 min-[450px]:gap-14 sm:mx-10 md:mx-20 md:justify-center md:gap-24"
             >
                 {{-- Packages count --}}
                 <div class="space-y-0.5">
@@ -95,6 +104,69 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+    {{-- Sponsor Card --}}
+    <div class="gsap-sponsor-card px-5 pt-5 sm:px-10">
+        <div
+            class="group/sponsor-card mx-auto grid w-full max-w-3xl place-items-center transition duration-300 will-change-transform hover:translate-x-0.5"
+        >
+            <div
+                class="h-full w-full rounded-[1.6rem] bg-gradient-to-br transition-colors duration-300 [grid-area:1/-1] dark:from-fuchsia-600/5 dark:via-fuchsia-600/5 dark:to-fuchsia-600/40"
+            ></div>
+            <a
+                href="https://github.com/sponsors/HassanZahirnia"
+                target="_blank"
+                class="flex h-[calc(100%-0.2rem)] w-[calc(100%-0.2rem)] items-center justify-between gap-10 overflow-hidden rounded-3xl bg-[#edf0fd] px-10 py-5 shadow-[8.05051px_24.1515px_89.4501px_-11.6285px_rgba(22,52,80,0.05)] backdrop-blur-xl transition-colors duration-300 [grid-area:1/-1] hover:bg-[#f1f3fe] dark:bg-[#101332] dark:backdrop-blur-lg hover:dark:bg-[#110E26]/40"
+            >
+                <div class="space-y-1.5">
+                    {{-- Title --}}
+                    <div class="flex items-center gap-2">
+                        <div
+                            class="text-xl font-bold text-[#040404] transition duration-300 dark:text-white"
+                        >
+                            Sponsor me on Github
+                        </div>
+                        <div
+                            class="-translate-x-1 scale-x-90 opacity-0 transition duration-300 group-hover/sponsor-card:translate-x-0 group-hover/sponsor-card:scale-x-100 group-hover/sponsor-card:opacity-100 motion-reduce:transition-none"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="22"
+                                height="22"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M4 12h16m0 0l-6-6m6 6l-6 6"
+                                ></path>
+                            </svg>
+                        </div>
+                    </div>
+                    {{-- Description --}}
+                    <div
+                        class="font-medium text-[#6A7789] transition duration-300 dark:text-[#96A5BB] sm:max-w-md"
+                    >
+                        To support this project!
+                    </div>
+                </div>
+                <div class="relative grid w-20 place-items-center">
+                    <img
+                        src="@viteAsset('resources/images/heart-bottle.webp')"
+                        width="50"
+                        height="80"
+                        alt=""
+                        class="pointer-events-none select-none mix-blend-hard-light dark:mix-blend-normal"
+                    />
+                    <div
+                        class="absolute right-1/2 top-1/2 -z-10 size-20 -translate-y-1/2 translate-x-1/2 bg-fuchsia-500 opacity-30 blur-xl dark:bg-rose-500 dark:opacity-50"
+                    ></div>
+                </div>
+            </a>
         </div>
     </div>
 </div>
